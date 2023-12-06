31936e1a1367e16486fa02a708ce504cf38f33c0
kdmin0706 <gxxk76@gmail.com>
2023-12-06 09:58:45 +0900

    user detail commit

--- src/main/java/com/springboot/security/data/entity/User.java
+++ src/main/java/com/springboot/security/data/entity/User.java
@@ -1,7 +1,7 @@
 package com.springboot.security.data.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
